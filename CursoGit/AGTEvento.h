b7f8cbb1d7c69cd8b6976b744be36d6877f0284b
Fernando Rodriguez <fernando@agbo.biz>
2013-03-16 12:21:11 +0100

    Añadimos clase evento vacía

---
+++ CursoGit/AGTEvento.h
@@ -0,0 +1,13 @@
+//
+//  AGTEvento.h
+//  CursoGit
+//
+//  Created by Fernando Rodríguez Romero on 3/16/13.
+//  Copyright (c) 2013 Agbo. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AGTEvento : NSObject
+
+@end
---
+++ CursoGit/AGTEvento.m
@@ -0,0 +1,13 @@
+//
+//  AGTEvento.m
+//  CursoGit
+//
+//  Created by Fernando Rodríguez Romero on 3/16/13.
+//  Copyright (c) 2013 Agbo. All rights reserved.
+//
+
+#import "AGTEvento.h"
+
+@implementation AGTEvento
+
+@end
