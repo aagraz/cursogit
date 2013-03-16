6911fbd24ecd17fbf4bc3f0d04900b9777d0d145
Fernando Rodriguez <fernando@agbo.biz>
2013-03-16 15:25:36 +0100

    Añadimos dirección

--- CursoGit/AGTAlumno.h
+++ CursoGit/AGTAlumno.h
@@ -14,5 +14,6 @@
 @property (nonatomic) BOOL registrado;
 @property (nonatomic, copy) NSString *apellidos;
 @property (nonatomic, copy) NSString *telefono;
+@property (nonatomic, copy) NSString *direccion;
 
 @end
